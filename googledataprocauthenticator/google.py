b66ecdef928697dc91e83e583d8f6d7502415994
alexismacaskilll <alexismacaskill2@gmail.com>
2020-08-31 09:19:34 -0700

    ipyvuetify

--- googledataprocauthenticator/google.py
+++ googledataprocauthenticator/google.py
@@ -449,11 +449,37 @@ class GoogleAuth(Authenticator):
             print(get_regions())
             self._update_region_list.options = get_regions()
     
-    def _update_cluster_list(self, widget, event, data):
+    def _update_cluster_list_from_region(self, widget, event, data):
         print(self.region_combobox.value)
         print(data)
-      
-        if data is not '' and data is not None:
+
+        #if a region is selected. 
+        if widget.label == 'Region' and data is not '' and data is not None:
+            #region = change['new']
+            print(data)
+            print(event)
+            #what error if the region is not valid? 
+            client = dataproc_v1beta2.ClusterControllerClient(credentials=self.credentials,
+                        client_options={
+                            "api_endpoint": f"{data}-dataproc.googleapis.com:443"
+                        }
+                    )
+            print(self.project_widget.value)
+            self.cluster_combobox.placeholder = _SELECT_CLUSTER_MESSAGE
+            self.filter_combobox.placeholder = _SELECT_FILTER_MESSAGE
+            print(self.filter_by_label.value)
+            self.cluster_combobox.options, self.filter_combobox.options = get_cluster_pool(self.project_widget.value, self.region_dropdown.value, client)
+        if widget.label == 'Filter by label' and data is not '' and data is not None:
+                #self.cluster_dropdown.options, self.filter_by_label.options = get_cluster_pool(self.project_widget.value, region, client)
+                _, self.cluster_combobox.options = get_cluster_pool(self.project_widget.value, data, client, self.filter_combobox.value)
+            
+
+    def _update_cluster_list_from_filter(self, widget, event, data):
+        print(self.region_combobox.value)
+        print(data)
+
+        #if a region is selected. 
+        if widget.label == 'Region' and data is not '' and data is not None:
             #region = change['new']
             print(data)
             print(event)
@@ -477,7 +503,7 @@ class GoogleAuth(Authenticator):
                 self.cluster_combobox.options, self.filter_combobox.options = get_cluster_pool(self.project_widget.value, self.region_dropdown.value, client)
 
             else:
-                _, self.cluster_combobox.options = get_cluster_pool(self.project_widget.value, self.region_dropdown.value, client, self.filter_combobox.value)
+                _, self.cluster_combobox.options = get_cluster_pool(self.project_widget.value, data, client, self.filter_combobox.value)
             
     
     # def _update_cluster_list(self, change):
